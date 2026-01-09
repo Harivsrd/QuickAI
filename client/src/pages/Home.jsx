7acd5c73700f7bc689b4b60e2efaf0d70d73feab
Harivsrd <harivsrdesineedi@gmail.com>
2026-01-09 17:50:14 +0530

    created user authentication using  clerk

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,9 @@
+import { Navbar } from "../components/Navbar";
+
 export function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <>
+        <Navbar />
+        </>
     )
 }
