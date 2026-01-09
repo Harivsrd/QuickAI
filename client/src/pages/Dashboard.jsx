26e866db4b9b00a14aebc4f7d525d04f55158913
Harivsrd <harivsrdesineedi@gmail.com>
2026-01-09 22:09:08 +0530

    Sidebar UI

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,4 +1,17 @@
+import { useEffect, useState } from "react"
+import { dummyCreationData } from "../assets/assets";
+
 export function Dashboard() {
+    const [creations,setCreations] = useState([]);
+
+    const getDashboardData = async  ()=>{
+        setCreations(dummyCreationData)
+    }
+
+    useEffect(()=>{
+        
+    },[])
+
     return (
         <div>
             <h1>Dashboard</h1>
